1ea637934bb1742d5b23004259689e0cc00d133c
Jonas Dorfinger <jonas.dorfinger@gmx.at>
2020-06-16 09:15:02 +0200

    format

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ app.use(express.static('public/script'));
 
 io.on('connection', (socket) => {
     console.log('a user connected');
-    
+
     socket.on('disconnect', () => {
-      console.log('user disconnected');
+        console.log('user disconnected');
     });
-  });
+});
 
 http.listen(3000, () => {
     console.log('listening on *:3000');
