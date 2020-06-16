21f52c5722e21470571796ba35827f6ac68f397e
Jonas Dorfinger <jonas.dorfinger@gmx.at>
2020-06-16 09:33:54 +0200

    add listener for new messages

--- public/script/chat.js
+++ public/script/chat.js
@@ -6,3 +6,10 @@ submitButton.addEventListener('click', (event) => {
     event.preventDefault();
     socket.emit('chat message', document.getElementById('message').value);
 });
+
+socket.on('chat message broadcast', (msg) => {
+    const li = document.createElement('li');
+    li.textContent = msg;
+
+    document.getElementById('messages').appendChild(li);
+});
