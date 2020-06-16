ef409d4bfc945e22ec2a01f32830acd474920f11
Jonas Dorfinger <jonas.dorfinger@gmx.at>
2020-06-16 09:00:00 +0200

    install npm module socket.io

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = require('express')();
 const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hello world</h1>');
+io.on('connection', (socket) => {
+    console.log('a user connected');
 });
 
 http.listen(3000, () => {
