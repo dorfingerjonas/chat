630273ebecde1da8bfd3c19039e4c9efb0499b80
Jonas Dorfinger <jonas.dorfinger@gmx.at>
2020-06-16 08:54:50 +0200

    use express static

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+const express = require('express');
 const app = require('express')();
 const http = require('http').createServer(app);
+
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.send('<h1>Hello world</h1>');
